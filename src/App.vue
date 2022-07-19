a66da2a65e53c280974f7a4e163b0de6eb46f972
Marin Skorsur <m.skorsur@gmail.com>
2022-07-19 14:15:18 +0200

    Add vuetify3 beta as dependency

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,19 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello from Tailor content element" />
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>Tailor content element SDK</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="2"></v-col>
+          <v-col cols="5"></v-col>
+          <v-col cols="5"></v-col>
+        </v-row>
+      </v-container>
+      <HelloWorld msg="Hello from Tailor custom element 😃"/>
+    </v-main>
+  </v-app>
 </template>
