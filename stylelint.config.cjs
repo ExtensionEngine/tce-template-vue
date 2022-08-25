a6c50463b25aff8be5675aa78f33daadd54229f9
Marin Skorsur <m.skorsur@gmail.com>
2022-08-25 11:47:39 +0200

    Update eslint configuration (#2)

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: '@extensionengine/stylelint-config'
+  extends: "@extensionengine/stylelint-config",
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vuetify from 'vite-plugin-vuetify';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src/preview/',
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true })
-  ],
+  root: "./src/preview/",
+  plugins: [vue(), vuetify({ autoImport: true })],
   build: {
     lib: {
-      entry: '../content-element/index.ts',
-      name: 'TCE',
-      fileName: 'content-element'
+      entry: "../content-element/index.ts",
+      name: "TCE",
+      fileName: "content-element",
     },
     rollupOptions: {
       output: {
-        dir: './dist'
-      }
-    }
-  }
+        dir: "./dist",
+      },
+    },
+  },
 });
